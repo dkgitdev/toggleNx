2c1bb79f9a19419fc8606170ca3e302f1e11797f
Dmitry Arkhipenko <me@dkdev.ru>
2022-12-04 00:20:51 +0300

    Fix: add tests for file

--- src/toggleNx.nim
+++ src/toggleNx.nim
@@ -2,8 +2,37 @@
 # exports the main API in this file. Note that you cannot rename this file
 # but you can remove it if you wish.
 
-import db
+import db/db
+import db/c
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+proc testFileWrite*(filename: cstring, content: cstring): void {.exportc, raises: [].} = 
+  var s: string = "Nim chnaged!"
+  echo s
+  echo "args: " & $filename & ", " & $content
+  when defined(posix) and not defined(nimscript):
+    echo "posix defined!"
+  when defined(nintendoswitch):
+    echo "nintendoswitch defined!"
+  try:
+    echo "try"
+    var f = open($filename, fmWrite)
+    echo "opened"
+    f.write(content)
+    echo "writen"
+    f.close()
+    echo "closed"
+
+
+    f = open($filename)
+    echo "opened again"
+    echo "read: " & f.readAll()
+    f.close()
+    echo "closed"
+    # f.write(content)
+    s = "OK"
+    echo "s = ok"
+  except Exception as e:
+    echo "exception"
+    s = "Cannot write file: " & $e.name & " " & e.msg
+  echo "final"
+  echo s
