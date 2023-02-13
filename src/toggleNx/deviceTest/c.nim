3e21035e1d48ec4420ca059ca953dd0d3a03e6d2
Dmitry Arkhipenko <me@dkdev.ru>
2023-02-13 23:58:12 +0300

    Add: device tests

--- src/toggleNx/deviceTest/c.nim
+++ src/toggleNx/deviceTest/c.nim
@@ -1,33 +1,38 @@
-# import chronicles
+import logging
+let consoleLogger = newConsoleLogger()
+let fileLogger = newFileLogger()
 
-# proc testFileWrite*(filename: cstring, content: cstring): void {.exportc, raises: [].} = 
-#   var s: string = "Nim chnaged!"
-#   try:
-#     debug "initial string", someString = s
-#     debug "params", filename = $filename, content = $content
-#     when defined(posix) and not defined(nimscript):
-#       debug "posix defined!"
-#     when defined(nintendoswitch):
-#       debug "nintendoswitch defined!"
-#     debug "try"
-#     var f = open($filename, fmWrite)
-#     debug "opened"
-#     f.write(content)
-#     debug "writen"
-#     f.close()
-#     debug "closed"
+addHandler(consoleLogger)
+addHandler(fileLogger)
+
+proc testDevice*(filename: cstring, content: cstring): void {.exportc, raises: [].} =
+  var s: string = "Nim chnaged!"
+  try:
+    debug "initial string: ", s
+    debug "params ", $filename, ", ", $content
+    when defined(posix) and not defined(nimscript):
+      debug "posix defined!"
+    when defined(nintendoswitch):
+      debug "nintendoswitch defined!"
+    debug "try"
+    var f = open($filename, fmWrite)
+    debug "opened"
+    f.write(content)
+    debug "writen"
+    f.close()
+    debug "closed"
 
 
-#     f = open($filename)
-#     debug "opened again"
-#     debug "read: ", contents = f.readAll()
-#     f.close()
-#     debug "closed"
-#     # f.write(content)
-#     s = "OK"
-#     debug "s = ok"
-#   except Exception as e:
-#     echo "exception"
-#     s = "Cannot write file: " & $e.name & " " & e.msg
-#   echo "final"
-#   echo s
+    f = open($filename)
+    debug "opened again"
+    debug "read: ", f.readAll()
+    f.close()
+    debug "closed"
+    # f.write(content)
+    s = "OK"
+    debug "s = ok"
+  except Exception as e:
+    echo "exception"
+    s = "Cannot write file: " & $e.name & " " & e.msg
+  echo "final"
+  echo s
--- toggleNx.nimble
+++ toggleNx.nimble
@@ -13,6 +13,7 @@ requires "nim >= 1.6.6"
 requires "switch_build >= 0.1.4"
 
 task test, "Runs the test suite":
+  exec "rm -rf build/"
   exec "nim c src/toggleNx/db/db.nim"
   exec "./build/db"
   exec "nim c src/toggleNx/db/c.nim"
@@ -21,7 +22,19 @@ task test, "Runs the test suite":
   exec "nimble build_nx"
 
 task build_pc, "Build PC C library":
+  exec "rm -rf build/"
   exec "nim c --noLinking:on --noMain:on src/toggleNx.nim"
 
 task build_nx, "Build NX C library":
+  exec "rm -rf build/"
   exec "switch_build -S src/toggleNx.nim"
+
+task build_device_test, "Build NX C library":
+  exec "rm -rf build/"
+  exec "switch_build -S src/toggleNx/deviceTest/c.nim"
+  cd("libs/nimNxStatic")
+  exec "make clean && make"
+  exec "mv *.elf ../../build"
+  exec "mv *.nacp ../../build"
+  exec "mv *.nro ../../build"
+  exec "rm -rf build"
